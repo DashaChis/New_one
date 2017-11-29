1472e6e66546abefefa39c0c89d169091f478941
DashaChis <31965164+DashaChis@users.noreply.github.com>
2017-11-29 13:08:18 +0300

    Update hw5.py

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,9 +1,9 @@
 a=str(input("Введите латинские слова:"))
 with open("text.txt", "w", encoding="utf-8") as f:
-        f.write('')
-while a!='':
+        f.write("")
+while a!="":
     if a[len(a)-3:]=="tur":
         with open("text.txt", "a", encoding="utf-8") as f:
                 f.write(a)
-                f.write('\n')
-    a=str(input())
+                f.write("\n")
+    a=str(input("Введите латинские слова:"))
