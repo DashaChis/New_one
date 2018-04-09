2909046b682feefab179be8a4153cb64a87cd1bf
DashaChis <31965164+DashaChis@users.noreply.github.com>
2018-04-09 16:38:08 +0300

    Create 2.py

---
+++ kontrolnaya/2.py
@@ -0,0 +1,27 @@
+import re
+def first():
+    d1 = {}
+    d2 = {}
+    l = []
+    a = 0
+    with open('var1.xml', encoding='utf-8') as f:
+        for line in f:
+            word = re.findall('type="[^"]*">', line)
+            word = str(word)
+            word = re.sub('type[=">]*', '', word)
+            l.append(word)
+    n = len(l)
+    for i in range (n):
+        b = l[i]
+        c = l.count(b)
+        d2 = {b : c}
+        d1.update(d2)
+    for key, value in d1.items():
+        print(key, value)
+    return d1
+
+def main():
+    first()
+                
+if __name__ == '__main__':
+    main()
