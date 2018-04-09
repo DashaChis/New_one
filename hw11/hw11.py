ccf51c5f4f7f2a6631e278e9c48ea0a3f5014324
DashaChis <31965164+DashaChis@users.noreply.github.com>
2018-04-10 01:49:03 +0500

    Update hw11.py

--- hw11/hw11.py
+++ hw11/hw11.py
@@ -6,8 +6,18 @@ def zavr():
     return words
 
 def change(words):
-    words = re.sub(r'\bДинозавр([ауеыо]?[мвх]?и?)\b', r'Кот\1', zavr())
-    words = re.sub(r'\bдинозавр([иауео]?[хмв]?и?)\b', r'кот\1', words)
+    words = re.sub(r'\bДинозавр([аыуе]?)\b', r'Кот\1', zavr())
+    words = re.sub(r'\bдинозавр([ыауе]?)\b', r'кот\1', words)
+    words = re.sub(r'\bДинозавр(ах)\b', r'кот\1', words)
+    words = re.sub(r'\bдинозавр(ах)\b', r'кот\1', words)
+    words = re.sub(r'\bДинозавр(ов)\b', r'Кот\1', words)
+    words = re.sub(r'\bдинозавр(ов)\b', r'кот\1', words)
+    words = re.sub(r'\bДинозавр(ами)\b', r'Кот\1', words)
+    words = re.sub(r'\bдинозавр(ами)\b', r'кот\1', words)
+    words = re.sub(r'\bДинозавр(ам)\b', r'Кот\1', words)
+    words = re.sub(r'\bдинозавр(ам)\b', r'кот\1', words)
+    words = re.sub(r'\bДинозавр(ом)\b', r'Кот\1', words)
+    words = re.sub(r'\bдинозавр(ом)\b', r'кот\1', words)
     return words
 
 def write(words):
@@ -19,4 +29,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    return words
